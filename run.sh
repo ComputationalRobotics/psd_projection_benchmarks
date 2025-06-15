1c835dae4102579e706f1c7b256cea3cb25a92d0
Antoine Groudiev <agroudiev@seas.harvard.edu>
2025-06-14 23:33:39 -0400

    changed restarts

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 datasets=("hilb" "rando" "sampling" "squared" "cubed")
 # instance_sizes=(10 20 50 100 1000 2000 5000 10000 20000)
 instance_sizes=(10 20 50 100 1000 2000 5000)
-restarts=1
+restarts=10
 gemmrestarts=25
 
 nvcc -std=c++17 -O3 -lcublas -lcudart -lcusolver -Wno-deprecated-gpu-targets -o benchmark benchmark.cu || exit 1
