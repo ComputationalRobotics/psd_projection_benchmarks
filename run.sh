e532b0ab435529d00b21060825b0948d1df01c03
Antoine Groudiev <agroudiev@seas.harvard.edu>
2025-06-28 14:49:04 -0400

    add psd_projection as submodule

--- run.sh
+++ run.sh
@@ -3,11 +3,8 @@
 # datasets=("sampling")
 # instance_sizes=(50 100 1000 2000 5000 10000 20000)
 # instance_sizes=(50 100 1000)
-# instance_sizes=(100)
 
 # datasets=("rohess" "frank" "golub" "hilb" "rando" "sampling" "squared" "cubed" "triw" "magic" "lotkin" "parter" "grcar")
-# datasets=("rohess" "frank" "golub" "hilb" "rando" "sampling" "squared" "cubed" "triw" "magic" "lotkin" "parter" "grcar")
-# instance_sizes=(5000)
 
 # full tests
 # datasets=("binomial" "cauchy" "chebspec" "chow" "circul" "clement" "companion" "dingdong" "fiedler" "forsythe" "frank" "golub" "grcar" "hadamard" "hankel" "hilb" "invhilb" "invol" "kahan" "kms" "lehmer" "lotkin" "magic" "minij" "moler" "neumann" "oscillate" "parter" "pascal" "pei" "poisson" "prolate" "randcorr" "rando" "randsvd" "rohess" "rosser" "sampling" "toeplitz" "tridiag" "triw" "vand" "wathen" "wilkinson")
@@ -15,17 +12,21 @@ datasets=("cauchy" "chebspec" "chow" "circul" "clement" "companion" "dingdong" "
 
 # removed: binomial (overflow), hadamard (power of 2), invhilb (overflow), neumann (sparse), poisson (sparse), rosser (power of 2), wathen (sparse), invol (overflow), pascal (overflow), vand (overflow)
 
-# # for debug
-# datasets=("parter")
-
 instance_sizes=(1000 2000 5000)
 # instance_sizes=(10000 20000)
 
 restarts=1
 gemmrestarts=1
 
-nvcc -std=c++17 -O3 -lcublas -lcudart -lcusolver -Wno-deprecated-gpu-targets -o benchmark benchmark.cu || exit 1
+# build the psd_projection library
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+cd psd_projection && cmake -S . -B build && cmake --build build && cd .. || exit 1
 
+# build the benchmark executable
+nvcc -std=c++17 -O3 -L. -Ipsd_projection/include -lcublas -lcudart -lcusolver -Wno-deprecated-gpu-targets -o benchmark benchmark.cu || exit 1
+
+# generate the datasets
 # julia data/generate.jl --datasets "${datasets[@]}" --instance_sizes "${instance_sizes[@]}" || exit 1
 
+# run the benchmark
 ./benchmark --datasets "${datasets[@]}" --instance_sizes "${instance_sizes[@]}" --restarts "$restarts" --gemmrestarts "$gemmrestarts"
